c8aac786c2c86ebb398a0cca9543b56e7b61faa4
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-04-19 20:27:58 -0500

    Página 2 del curriculum

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,104 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonThumbnail,
+  IonSegment,
+  IonSegmentButton,
+  IonAvatar,
+  IonChip,
+  IonBadge,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonProgressBar,
+  IonRange,
+  IonAccordion,
+  IonAccordionGroup,
+  IonToggle,
+  IonText
+} from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import {
+  school,
+  person,
+  code,
+  download,
+  call,
+  mail,
+  location
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonThumbnail,
+
+    IonSegment,
+    IonSegmentButton,
+
+    IonAvatar,
+    IonChip,
+    IonBadge,
+    IonIcon,
+
+    IonGrid,
+    IonRow,
+    IonCol,
+
+    IonProgressBar,
+    IonRange,
+
+    IonAccordion,
+    IonAccordionGroup,
+    IonToggle,
+
+    IonText
+  ]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor() {
+    addIcons({
+      school,
+      person,
+      code,
+      download,
+      call,
+      mail,
+      location
+    });
+  }
 
 }
