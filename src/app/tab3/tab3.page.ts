267ce9294462c80516882a4144f67ff2cf8b1ba9
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-04-19 20:48:09 -0500

    Página 3 del curriculum

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,64 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonList,
+  IonAvatar,
+  IonText,
+  IonNote,
+  IonFab,
+  IonFabButton
+} from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { call, mail, location, logoWhatsapp } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+
+    IonItem,
+    IonLabel,
+    IonIcon,
+    IonList,
+    IonAvatar,
+    IonText,
+    IonNote,
+
+    IonFab,
+    IonFabButton
+  ]
 })
 export class Tab3Page {
-  constructor() {}
+
+  constructor() {
+    addIcons({
+      call,
+      mail,
+      location,
+      logoWhatsapp
+    });
+  }
+
 }
